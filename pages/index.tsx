0fd73f8390571b12f42bec97a102b61de9e1a47d
Kayla <99969901+luvKxylah@users.noreply.github.com>
2022-05-22 08:53:39 +0100

    use lanyard to fetch location

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import { SiGithub as GitHubIcon } from 'react-icons/si'
 import { HiOutlineLocationMarker as LocationMarker } from 'react-icons/hi'
+import { Data as lanyardData, useLanyard } from 'use-lanyard'
 
 function currentAge(): number {
   const birthday = new Date('28 November 2006')
@@ -10,7 +11,15 @@ function currentAge(): number {
   return Math.abs(ageDate.getUTCFullYear() - 1970)
 }
 
-const Home: NextPage = () => {
+interface Props {
+  lanyard: lanyardData
+}
+
+export default function Home(props: Props) {
+  const { data: lanyard } = useLanyard('971862569792987147', {
+    fallbackData: props.lanyard,
+  })
+
   return (
     <>
       <Head>
@@ -27,21 +36,23 @@ const Home: NextPage = () => {
             <GitHubIcon className="h-7 w-7" />
             <span className="sr-only">GitHub Profile</span>
           </a>
-          <p className="inline-flex items-center space-x-2">
-            <a
-              target="_blank"
-              href={`https://www.google.com/maps/search/Lisbon,%20Portugal`}
-              rel="noreferrer"
-              className="hover:bg flex items-center rounded-full bg-gray-200 px-2 pr-3 text-neutral-600 no-underline transition-colors hover:bg-gray-300"
-            >
-              <span>
-                <LocationMarker className="inline" />
-                &nbsp;
-              </span>
+          {lanyard && (
+            <p className="inline-flex items-center space-x-2">
+              <a
+                target="_blank"
+                href={`https://www.google.com/maps/search/${lanyard.kv.location}`}
+                rel="noreferrer"
+                className="hover:bg flex items-center rounded-full bg-gray-200 px-2 pr-3 text-neutral-600 no-underline transition-colors hover:bg-gray-300"
+              >
+                <span>
+                  <LocationMarker className="inline" />
+                  &nbsp;
+                </span>
 
-              <span className="-mb-0.5">Lisbon, Portugal</span>
-            </a>
-          </p>
+                <span className="-mb-0.5">{lanyard.kv.location}</span>
+              </a>
+            </p>
+          )}
         </div>
 
         <h1 className="text-3xl font-bold sm:text-4xl md:text-6xl">
@@ -55,5 +66,3 @@ const Home: NextPage = () => {
     </>
   )
 }
-
-export default Home
